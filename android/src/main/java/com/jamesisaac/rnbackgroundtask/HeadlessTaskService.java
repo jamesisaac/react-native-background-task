979b5aca9aaa2c6b64b169da3e226501bfaeb65e
Evgeny Evstropov <evgeny@getcodified.com>
2018-11-20 16:02:43 +0300

    call startForeground inside onCreate for android >=8

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/HeadlessTaskService.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/HeadlessTaskService.java
@@ -9,9 +9,33 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 import com.facebook.react.HeadlessJsTaskService;
 import java.util.concurrent.TimeUnit;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 
 public class HeadlessTaskService extends HeadlessJsTaskService {
     private static final String TAG = "BackgroundTask";
+
+    @Override
+    @SuppressLint("WrongConstant")
+    public void onCreate() {
+        super.onCreate();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Context mContext = this.getApplicationContext();
+            String CHANNEL_ID = "Background job";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_LOW);
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification =
+                    new Notification.Builder(mContext, CHANNEL_ID)
+                            .setContentTitle("Running background job")
+                            .setContentText(mContext.getPackageName())
+                            .build();
+            startForeground(1, notification);
+        }
+    }
 
     @Override
     protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
