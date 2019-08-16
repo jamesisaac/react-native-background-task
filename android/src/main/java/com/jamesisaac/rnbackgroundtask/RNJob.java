d167c30542873dfd98f3f2b5c3f8a6122d95ae74
jgtoriginal <jgtoriginal@users.noreply.github.com>
2019-08-16 15:57:58 -0300

    upgrading to react-native 0.60

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/RNJob.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/RNJob.java
@@ -3,7 +3,7 @@ package com.jamesisaac.rnbackgroundtask;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import com.evernote.android.job.util.support.PersistableBundleCompat;
 import com.evernote.android.job.Job;
