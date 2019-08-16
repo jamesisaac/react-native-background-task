fd3fe848e39649f05900c0e6f2ad4f1b30300973
jgtoriginal <jgtoriginal@users.noreply.github.com>
2019-08-16 15:57:27 -0300

    upgrading to react-native 0.60

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/HeadlessTaskService.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/HeadlessTaskService.java
@@ -2,7 +2,7 @@ package com.jamesisaac.rnbackgroundtask;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
